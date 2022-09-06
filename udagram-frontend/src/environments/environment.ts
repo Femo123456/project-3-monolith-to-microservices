65b6e9930582610a32ce5b09b73a8e83893fc8fb
Faheem Ismail <femo.ismail@gmail.com>
2022-09-06 17:38:23 +0200

    Final update

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a95eae64c146f454fbdad9a6be30d0be-204422941.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
